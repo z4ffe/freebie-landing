5e146903dae38f55c70fc092df654e50d9f1499f
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2022-07-01 15:21:52 +0300

    fix: fix some things

---
+++ script/script.js
@@ -0,0 +1,17 @@
+/*
+const card = document.querySelectorAll('.card')
+
+
+card.forEach(function (card){
+    card.addEventListener('click', function (){
+        console.log('asd')
+    })
+})
+
+
+*/
+/*
+
+const cardStyle = function () {
+
+}*/
