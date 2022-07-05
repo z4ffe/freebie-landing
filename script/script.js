09fb34a5d1b300bee136677ff7f4e27272aa7c35
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2022-07-05 17:17:14 +0300

    feat: add modal window and animations

--- script/script.js
+++ script/script.js
@@ -15,3 +15,24 @@ card.forEach(function (card){
 const cardStyle = function () {
 
 }*/
+
+
+const modalWindow = document.querySelector('#modal')
+const modalWrapper = document.querySelector('#modal__wrapper')
+const modalExit = document.querySelector('#modal__exit')
+const SignupButton = document.querySelector('#btn-signup')
+
+
+modalExit.addEventListener('click', function () {
+    modalWindow.classList.remove('modal--active')
+    modalWrapper.classList.remove('modal-wrapper--active')
+})
+
+SignupButton.addEventListener('click', function (){
+    modalWindow.classList.add('modal--active')
+    modalWrapper.classList.add('modal-wrapper--active')
+})
+
+
+console.log(modalWindow.classList.value)
+
